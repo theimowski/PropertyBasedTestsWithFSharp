574e7e62c84b06cef37fe4e5dfdd1473c33151a6
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-06-12 16:02:50 +0200

    zero out demo module

--- demo/Library/LiveDemo.fs
+++ demo/Library/LiveDemo.fs
@@ -1,26 +1,9 @@
 ﻿module Calc
 
 let add (x : int) (y : int) =
-        x ||| y
+        x - y
 
 open Xunit
 open FsCheck
 open FsCheck.Xunit
 open Swensen.Unquote
-
-[<Property>]
-let ``add is commutative`` (x, y) = 
-    test <@ add x y = add y x @>
-
-[<Property>]
-let ``add is associative`` (x, y, z) = 
-    test <@ add (add x y) z = add x (add y z) @>
-
-[<Property>]
-let ``0 is the identity element for add`` (x) =
-    test <@ add x 0 = x @>
-
-[<Property>]
-let ``if both operands > 0 then result also`` (x,y) =
-    if x > 0 && y > 0 then
-        test <@ add x 0 = x @>
